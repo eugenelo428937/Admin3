6011b798743aab49b7d46c712cef4a5e04574d27
eugenelo428937 <eugenelo@bpp.com>
2024-10-09 11:06:23 +0100

    Revert "Please enter the commit message for your changes. Lines starting"
    This reverts commit 590b7c1f4ffaa13af221e427e294fdc1e59a77f6.

--- backend/Admin3/students/urls.py
+++ backend/Admin3/students/urls.py
@@ -1,9 +1,8 @@
-# students/urls.py
+# myapp/urls.py
 
 from django.urls import path
-from .views import create_user, create_student
+from .views import create_user
 
 urlpatterns = [
     path('create_user/', create_user, name='create_user'),
-    path('create_student/', create_student, name='create_student'),
 ]
