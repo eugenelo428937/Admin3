1ad7cccd8b1fa08421ac9223c9f257fcc85a6f58
eugenelo428937 <eugenelo@bpp.com>
2024-10-15 14:38:48 +0100

    20241015

---
+++ frontend/react-Admin3/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from "react";
+import StudentForm from "./StudentForm";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Add a New Student</h1>
+            <StudentForm />
+        </div>
+    );
+};
+
+export default App;
