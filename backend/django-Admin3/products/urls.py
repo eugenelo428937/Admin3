dc4d1f4d890a5c1f7936b9fc9410843bc409f825
eugenelo428937 <eugenelo@bpp.com>
2025-03-11 13:08:46 +0000

    20250311

--- backend/django-Admin3/products/urls.py
+++ backend/django-Admin3/products/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet, basename='product')
+router.register('', ProductViewSet, basename='product')
 
 app_name = 'products'
 
