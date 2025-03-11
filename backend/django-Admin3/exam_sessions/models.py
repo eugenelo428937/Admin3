b297ff54271dcd123fd570bbe3d7af9c670cc8f9
eugenelo428937 <eugenelo@bpp.com>
2025-03-11 13:08:46 +0000

    20250311

--- backend/django-Admin3/exam_sessions/models.py
+++ backend/django-Admin3/exam_sessions/models.py
@@ -15,20 +15,3 @@ class ExamSession(models.Model):
 
     def __str__(self):
         return f"{self.session_code} ({self.start_date} - {self.end_date})"
-# exam_sessions/models.py
-from django.db import models
-
-class ExamSession(models.Model):
-    session_code = models.CharField(max_length=50, null=False)
-    start_date = models.DateTimeField(null=False)
-    end_date = models.DateTimeField(null=False)
-    create_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = 'acted_exam_sessions'  # Added "acted_" prefix
-        verbose_name = 'Exam Session'
-        verbose_name_plural = 'Exam Sessions'
-
-    def __str__(self):
-        return f"{self.session_code} ({self.start_date} - {self.end_date})"
