1059285989aae82280049dcf6c32046b7144a78a
eugenelo428937 <eugenelo@bpp.com>
2025-03-27 17:22:43 +0000

    20250327

--- backend/django-Admin3/django-Admin3/wsgi.py
+++ backend/django-Admin3/django-Admin3/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-Admin3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'django-Admin3.settings.production')
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-Admin3.settings')
 
 application = get_wsgi_application()
