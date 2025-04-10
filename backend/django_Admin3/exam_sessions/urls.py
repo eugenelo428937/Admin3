9404deeffa213caf479dc715ae960d55047fc22d
eugenelo428937 <eugenelo@bpp.com>
2025-04-10 09:38:51 +0100

    20240410

--- backend/django_Admin3/exam_sessions/urls.py
+++ backend/django_Admin3/exam_sessions/urls.py
@@ -2,12 +2,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ExamSessionViewSet
-from . import views
 
 router = DefaultRouter()
-router.register(r'exam-sessions', ExamSessionViewSet, basename='exam-sessions')  # Add basename parameter
+router.register('', ExamSessionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('csrf/', views.get_csrf_token, name='csrf'),
+    path('', include(router.urls))
 ]
--- frontend/react-Admin3/src/services/examSessionService.js
+++ frontend/react-Admin3/src/services/examSessionService.js
@@ -1,37 +1,40 @@
 import httpService from "./httpService";
 import config from "../config";
 const API_URL = config.examSessionUrl;
-
+console.log("API Base examSessionUrl:", API_URL);
 const examSessionService = {
-    getAll: async () => {
-        const response = await httpService.get(`${API_URL}`);
-        return response.data;
-    },
+	getAll: async () => {
+		try {			
+			const response = await httpService.get(`${API_URL}/`);
+			console.log(response.data)
+			// Ensure we're returning an array
+			if (!response.data) return [];
+			return Array.isArray(response.data) ? response.data : response.data.results || Object.values(response.data) || [];
+		} catch (error) {
+			console.error("Error fetching Exam Sessions:", error);
+			return [];
+		}
+	},
 
-    getById: async (id) => {
-        const response = await httpService.get(`${API_URL}${id}/`);
-        return response.data;
-    },
+	getById: async (id) => {
+		const response = await httpService.get(`${API_URL}/${id}/`);
+		return response.data;
+	},
 
-    create: async (examSession) => {
-        const response = await httpService.post(
-            `${API_URL}`, 
-            examSession
-        );
-        return response.data;
-    },
+	create: async (examSession) => {
+		const response = await httpService.post(`${API_URL}/`, examSession);
+		const response2 = await httpService.post(`${config.examSessionSubjectUrl}/insert-subjects/`, examSession);
+		return response.data;
+	},
 
-    update: async (id, examSession) => {
-        const response = await httpService.put(
-            `${API_URL}${id}/`, 
-            examSession
-        );
-        return response.data;
-    },
+	update: async (id, examSession) => {
+		const response = await httpService.put(`${API_URL}/${id}/`, examSession);
+		return response.data;
+	},
 
-    delete: async (id) => {
-        await httpService.delete(`${API_URL}${id}/`);
-    }
+	delete: async (id) => {
+		await httpService.delete(`${API_URL}${id}/`);
+	},
 };
 
 export default examSessionService;
