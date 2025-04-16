eb0ceafa81f7afb81c646d3dda6cf22c197ab385
eugenelo428937 <eugenelo@bpp.com>
2025-04-16 17:19:30 +0100

    20250416

--- backend/django_Admin3/django_Admin3/settings/base.py
+++ backend/django_Admin3/django_Admin3/settings/base.py
@@ -237,14 +237,14 @@ CORS_ALLOW_HEADERS = [
 AUTH_USER_MODEL = 'auth.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    ],    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50
 }
