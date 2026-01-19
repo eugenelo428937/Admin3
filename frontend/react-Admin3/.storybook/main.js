e4a01a644f9fcbf581f3a7463550e8945352b332
eugenelo428937 <eugenelo@bpp.com>
2026-01-19 10:29:00 +0000

    feat(storybook): integrate Storybook for enhanced component development
    - Added Storybook configuration to the project, including recommended plugins for accessibility and documentation.
    - Updated ESLint configuration to include Storybook rules for improved linting during development.
    - Enhanced .gitignore to exclude Storybook build artifacts and logs.
    - Updated package dependencies to include Storybook and related addons for a comprehensive development experience.
    
    These changes aim to streamline the component development process and improve collaboration through visual documentation.

---
+++ frontend/react-Admin3/.storybook/main.js
@@ -0,0 +1,21 @@
+
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "storybook-design-token"
+  ],
+  "framework": "@storybook/react-webpack5",
+  "staticDirs": [
+    "..\\public"
+  ]
+};
+export default config;
